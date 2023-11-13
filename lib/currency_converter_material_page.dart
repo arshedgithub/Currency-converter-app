64d05983fc04c377d6dfcfd3f014284930ea04e2
arshedgithub <arshedahmedmail@gmail.com>
2023-11-13 06:42:15 +0530

    add text field

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,9 +5,59 @@ class CurrenceyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
+    // final border = 
+
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text('Hello World !!!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Please Enter the amount in USD',
+                  hintStyle: TextStyle(
+                    color: Colors.black
+                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
